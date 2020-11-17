fd687ad7b4d7e530ba4da05e703cbc3fa38c89c2
seeris <sergei.udris@gmail.com>
2020-11-17 22:53:50 +0300

    build: rename peer to peernode

---
+++ src/peernode/src/deathstar/peernode/main.cljs
@@ -0,0 +1,25 @@
+(ns deathstar.peernode.main
+  (:require
+   [clojure.core.async :as a :refer [chan go go-loop <! >!  take! put! offer! poll! alt! alts! close!
+                                     pub sub unsub mult tap untap mix admix unmix pipe
+                                     timeout to-chan  sliding-buffer dropping-buffer
+                                     pipeline pipeline-async]]
+   [cljs.core.async.interop :refer-macros [<p!]]
+   [goog.string.format :as format]
+   [goog.string :refer [format]]
+   [goog.object]
+   [clojure.string :as string]
+   [cljs.reader :refer [read-string]]
+   [cljs.nodejs :as node]
+
+   [cljctools.csp.op.spec :as op.spec]
+   [cljctools.cljc.core :as cljc.core]
+
+   [cljctools.rsocket.impl]
+   [cljctools.rsocket.impl.examples-nodejs]))
+
+(def fs (node/require "fs"))
+(def path (node/require "path"))
+
+(defn main []
+  (println ::main))
